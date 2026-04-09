81a465c8f2f0d1a5de119a33224e16d76c8351be
Simeon Markov <simeon.markov1994@gmail.com>
2026-04-09 16:16:40 +0300

    displaying category details for multiple categories for a hardcoded time period

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 import { Category } from "./Category.jsx";
 
@@ -15,79 +12,141 @@ function App() {
     Daily
     Weekly
     Monthly
-
-    Work
-    5hrs <!-- daily -->
-    Previous - 7hrs <!-- daily -->
-    32hrs <!-- weekly -->
-    Previous - 36hrs <!-- weekly -->
-    103hrs <!-- monthly -->
-    Previous - 128hrs <!-- monthly -->
-
-    Play
-    1hr <!-- daily -->
-    Previous - 2hrs <!-- daily -->
-    10hrs <!-- weekly -->
-    Previous - 8hrs <!-- weekly -->
-    23hrs <!-- monthly -->
-    Previous - 29hrs <!-- monthly -->
-
-    Study
-    0hrs <!-- daily -->
-    Previous - 1hr <!-- daily -->
-    4hrs <!-- weekly -->
-    Previous - 7hrs <!-- weekly -->
-    13hrs <!-- monthly -->
-    Previous - 19hrs <!-- monthly -->
-
-    Exercise
-    1hr <!-- daily -->
-    Previous - 1hr <!-- daily -->
-    4hrs <!-- weekly -->
-    Previous - 5hrs <!-- weekly -->
-    11hrs <!-- monthly -->
-    Previous - 18hrs <!-- monthly -->
-
-    Social
-    1hr <!-- daily -->
-    Previous - 3hrs <!-- daily -->
-    5hrs <!-- weekly -->
-    Previous - 10hrs <!-- weekly -->
-    21hrs <!-- monthly -->
-    Previous - 23hrs <!-- monthly -->
-
-    Self Care
-    0hrs <!-- daily -->
-    Previous - 1hr <!-- daily -->
-    2hrs <!-- weekly -->
-    Previous - 2hrs <!-- weekly -->
-    7hrs <!-- monthly -->
-    Previous - 11hrs <!-- monthly -->
-    *
-    * */
+*/
     const [categories, setCategories] = useState([
         {
-            id: 'card-exercise-1',
-            headingSrc: 'images\\icon-exercise.svg'
+            id: 'card-work-1',
+            headingSrc: 'src/assets/icon-work.svg',
+            categoryName: '',
+            hoursSpentForTime: [
+                {
+                    timeUnit: 'hours',
+                    currentTimeSpent: 5,
+                    previousTimeSpent: 7
+                },
+                {
+                    timeUnit: 'weeks',
+                    currentTimeSpent: 32,
+                    previousTimeSpent: 36
+                },
+                {
+                    timeUnit: 'months',
+                    currentTimeSpent: 103,
+                    previousTimeSpent: 128
+                }
+            ]
         },
         {
             id: 'card-play-1',
-            headingSrc: 'images\\icon-play.svg'
+            headingSrc: 'src/assets/icon-play.svg',
+            hoursSpentForTime: [
+                {
+                    timeUnit: 'hours',
+                    currentTimeSpent: 1,
+                    previousTimeSpent: 2
+                },
+                {
+                    timeUnit: 'weeks',
+                    currentTimeSpent: 10,
+                    previousTimeSpent: 8
+                },
+                {
+                    timeUnit: 'months',
+                    currentTimeSpent: 23,
+                    previousTimeSpent: 29
+                }
+            ]
         },
         {
-            id: 'card-self-care-1',
-            headingSrc: 'images\\icon-self-care.svg'
+            id: 'card-study-1',
+            headingSrc: 'src/assets/icon-study.svg',
+            hoursSpentForTime: [
+                {
+                    timeUnit: 'hours',
+                    currentTimeSpent: 0,
+                    previousTimeSpent: 1
+                },
+                {
+                    timeUnit: 'weeks',
+                    currentTimeSpent: 4,
+                    previousTimeSpent: 7
+                },
+                {
+                    timeUnit: 'months',
+                    currentTimeSpent: 13,
+                    previousTimeSpent: 19
+                }
+            ]
+        },
+        {
+            id: 'card-exercise-1',
+            headingSrc: 'src/assets/icon-exercise.svg',
+            hoursSpentForTime: [
+                {
+                    timeUnit: 'hours',
+                    currentTimeSpent: 1,
+                    previousTimeSpent: 1
+                },
+                {
+                    timeUnit: 'weeks',
+                    currentTimeSpent: 4,
+                    previousTimeSpent:5
+                },
+                {
+                    timeUnit: 'months',
+                    currentTimeSpent: 11,
+                    previousTimeSpent: 18
+                }
+            ]
         },
         {
             id: 'card-social-1',
-            headingSrc: 'images\\icon-social.svg'
+            headingSrc: 'src/assets/icon-social.svg',
+            hoursSpentForTime: [
+                {
+                    timeUnit: 'hours',
+                    currentTimeSpent: 1,
+                    previousTimeSpent: 1
+                },
+                {
+                    timeUnit: 'weeks',
+                    currentTimeSpent: 5,
+                    previousTimeSpent: 10
+                },
+                {
+                    timeUnit: 'months',
+                    currentTimeSpent: 21,
+                    previousTimeSpent: 23
+                }
+            ]
+        },
+        {
+            id: 'card-self-care-1',
+            headingSrc: 'src/assets/icon-self-care.svg',
+            hoursSpentForTime: [
+                {
+                    timeUnit: 'hours',
+                    currentTimeSpent: 0,
+                    previousTimeSpent: 1
+                },
+                {
+                    timeUnit: 'weeks',
+                    currentTimeSpent: 2,
+                    previousTimeSpent: 2
+                },
+                {
+                    timeUnit: 'months',
+                    currentTimeSpent: 7,
+                    previousTimeSpent: 11
+                }
+            ]
         }
     ])
 
   return (
    <>
        {categories.map(category => (
-           <Category key={category.id} headingSrc={category.headingSrc}/>
+           <Category key={category.id} categoryDetails = {category} />
        ))}
    </>
   )
--- src/Category.jsx
+++ src/Category.jsx
@@ -1,11 +1,15 @@
+import './Category.css'
+
 export function Category(props) {
     return (
-        <section>
+        <section className="category-container">
             <div className={"heading-banner"}>
-                <img src={props.headingSrc}/>
+                <img src={props.categoryDetails.headingSrc} alt={'picture'}/>
             </div>
             <div>
-
+                <p>{props.categoryDetails.categoryName}</p>
+                <p>{props.categoryDetails.hoursSpentForTime.find(timePeriod => timePeriod.timeUnit === "weeks").currentTimeSpent}hrs</p>
+                <p>Last week - {props.categoryDetails.hoursSpentForTime.find(timePeriod => timePeriod.timeUnit === "weeks").previousTimeSpent}hrs</p>
             </div>
         </section>
     );
